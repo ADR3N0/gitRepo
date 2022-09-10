11051668a75bc9cd97a24a9e8b86a08a7f748b0c
ADR3N0 <arruchaedu@gmail.com>
2022-09-10 16:56:25 -0500

    Commit inicial

---
+++ tecweb/practicas/p03/variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang=“es">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Practica 3</title>
+</head>
+
+<body>
+<h1 style=text-align:center;>Manejo de variables en PHP</h1>
+
+
+<?php
+
+?>
+
+</body>
+</html>
