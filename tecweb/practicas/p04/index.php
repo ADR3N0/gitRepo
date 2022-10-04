1dc6c612ae08a4371dee66e0b9e743ff31706713
ADR3N0 <arruchaedu@gmail.com>
2022-10-04 00:37:31 -0500

    Ejercicio 3 Completado p04

--- tecweb/practicas/p04/index.php
+++ tecweb/practicas/p04/index.php
@@ -56,7 +56,24 @@
         <p>Utiliza un ciclo <strong>while</strong> para encontrar el primer número entero obtenido aleatoriamente, pero que además sea múltiplo de un número dado.</p>
         <p>
         <strong>Ciclo While R:</strong>
-
+            <?php
+            if (!empty($_GET['numero'])) {
+                $numero = $_GET['numero'];
+                echo cicloWhile($numero);
+            } else {
+                echo 'No existen parametros en la URL';
+            }
+            ?>
+        </br>
+        <strong>Ciclo Do While R:</strong>
+            <?php
+            if (!empty($_GET['numero'])) {
+                $numero = $_GET['numero'];
+                echo cicloDoWhile($numero);
+            } else {
+                echo 'No existen parametros en la URL';
+            }
+            ?>
         </p>
     </div>
     <hr>
